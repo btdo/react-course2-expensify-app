47db97e525bf277e823b5d0ed104d88c3cc9c769
bao.do <bao.do@innovapost.com>
2018-07-28 21:50:27 -0400

    setup production webpack build

--- src/app.js
+++ src/app.js
@@ -11,7 +11,7 @@ import getVisibleExpenses from './selectors/expenses';
 import 'react-dates/lib/css/_datepicker.css';
 
 const store = configureStore();
-
+console.log('test');
 
 
 // setTimeout(() => {
